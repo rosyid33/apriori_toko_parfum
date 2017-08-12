5eb12cd1dc2a48b7607c6869a50a498ff4a60e94
rosyid_ido <rosyid33@gmail.com>
2017-08-12 20:56:23 +0700

    commit pertama

---
+++ footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <div class="footer-inner">
+        <div class="footer-content">
+            <span class="bigger-120">
+                <span class="blue bolder">Data Mining</span>
+                Apriori Toko Parfum &copy; 2017
+            </span>
+        </div>
+    </div>
+</div>
---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Configuration Database
+ */
+return array(
+    'host' => "localhost",
+    'username' => "root",
+    'password' => "",
+    'dbname' => "apriori_toko_parfum",
+    // 'port' => "", // sementara menggunakan port bawaan
+);
+
+?>
